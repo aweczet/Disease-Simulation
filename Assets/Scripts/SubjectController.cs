2974f5b15d93215eace4685da41ba8a680e1cb99
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-02 19:33:10 +0200

    add: basic prototype of subject movement

---
+++ Assets/Scripts/SubjectController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class SubjectController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Vector2 _moveDirection;
+    private float _speed;
+    private void Start()
+    {
+        _rigidbody = transform.GetComponent<Rigidbody2D>();
+        _speed = 10f;
+    }
+
+    private void Update()
+    {
+        _rigidbody.velocity = transform.up * _speed;
+    }
+}
