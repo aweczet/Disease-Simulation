6b1b3b539e492a2de5f42a4cb248cde1004558cd
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-02 19:37:48 +0200

    add: Time tick system

---
+++ Assets/Scripts/TimeTickSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class TimeTickSystem : MonoBehaviour
+{
+    public class OnTickEvents : EventArgs
+    {
+        public int Tick;
+    }
+
+    public static event EventHandler<OnTickEvents> OnTick;
+    
+    // Number of seconds per tick
+    private const float MAXTick = .5f;
+    private int _tick;
+    private float _tickTimer;
+
+    private void Awake()
+    {
+        _tick = 0;
+    }
+
+    private void Update()
+    {
+        _tickTimer += Time.deltaTime;
+        if (_tickTimer >= MAXTick)
+        {
+            _tickTimer -= MAXTick;
+            _tick++;
+            if (OnTick != null)
+            {
+                OnTick(this, new OnTickEvents()
+                {
+                    Tick = _tick
+                });
+            }
+        }
+    }
+}
