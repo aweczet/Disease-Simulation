f1ff86a54dae932c5713ba115e9b18478d844763
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-02 20:25:26 +0200

    add: random values for subject + immunity value check (by age)

--- Assets/Scripts/SubjectController.cs
+++ Assets/Scripts/SubjectController.cs
@@ -1,19 +1,63 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SubjectController : MonoBehaviour
 {
+    private EventHandler<TimeTickSystem.OnTickEvents> _tickSystemDelegate;
     private Rigidbody2D _rigidbody;
+    
     private Vector2 _moveDirection;
     private float _speed;
+
+    public StatusType status;
+    private int _age;
+    private float _immunity;
+    
     private void Start()
     {
         _rigidbody = transform.GetComponent<Rigidbody2D>();
-        _speed = 10f;
+        _tickSystemDelegate = delegate
+        {
+            status = (StatusType)Random.Range(0, Enum.GetValues(typeof(StatusType)).Length);
+            Debug.Log(status);
+        };
+        TimeTickSystem.OnTick += _tickSystemDelegate;
+
+        _speed = Random.Range(5f, 10f);
+        status = (StatusType)Random.Range(0, Enum.GetValues(typeof(StatusType)).Length);
+        Debug.Log(status);
     }
 
     private void Update()
     {
         _rigidbody.velocity = transform.up * _speed;
+        transform.Rotate(Vector3.forward, Space.Self);
+    }
+
+    private float CheckImmunity()
+    {
+        float retImmunity = 0;
+        
+        if ((_age >= 0 && _age < 15) || (_age >= 70 && _age <= 100))
+            retImmunity = _immunity > 3 ? Random.Range(0f, 3f) : _immunity;
+        else if (_age >= 15 && _age < 40)
+            retImmunity = _immunity > 6 ? Random.Range(3f, 6f) : _immunity;
+        else if (_age >= 40 && _age < 70)
+            retImmunity = _immunity;
+        
+        return retImmunity;
+    }
+    
+    
+
+    private void ChangeDirection()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        ChangeDirection();
     }
 }
