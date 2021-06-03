8c4101f5050c99398190c4f5f86902df53afe42c
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-03 14:19:49 +0200

    add: subject generator prototype

---
+++ Assets/Scripts/SubjectGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class SubjectGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private void Start()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
