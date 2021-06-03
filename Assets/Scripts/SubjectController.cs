2b5f3424fe29007b74c02e9a0e86b6f45aaa979a
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-03 13:39:55 +0200

    add: subjects change direction if collide with each other

--- Assets/Scripts/SubjectController.cs
+++ Assets/Scripts/SubjectController.cs
@@ -4,6 +4,7 @@ using Random = UnityEngine.Random;
 
 public class SubjectController : MonoBehaviour
 {
+    private Vector2 _maxBoardSize;
     private EventHandler<TimeTickSystem.OnTickEvents> _tickSystemDelegate;
     private Rigidbody2D _rigidbody;
 
@@ -16,22 +17,31 @@ public class SubjectController : MonoBehaviour
 
     private void Start()
     {
+        _maxBoardSize = new Vector2(8f, 4f);
         _rigidbody = transform.GetComponent<Rigidbody2D>();
         _tickSystemDelegate = delegate
         {
-            status = (StatusType) Random.Range(0, Enum.GetValues(typeof(StatusType)).Length);
+            // status = (StatusType) Random.Range(0, Enum.GetValues(typeof(StatusType)).Length);
             // Debug.Log(status);
         };
         TimeTickSystem.OnTick += _tickSystemDelegate;
-
-        _speed = Random.Range(5f, 10f);
-        status = (StatusType) Random.Range(0, Enum.GetValues(typeof(StatusType)).Length);
-        Debug.Log(status);
+        Init();
     }
 
     private void Update()
     {
         _rigidbody.velocity = transform.up * _speed;
+    }
+
+    private void Init()
+    {
+        // Set random position, rotation, speed, status, age and immunity value
+        transform.position = new Vector2(Random.Range(-_maxBoardSize.x, _maxBoardSize.x),
+            Random.Range(-_maxBoardSize.y, _maxBoardSize.y));
+        transform.Rotate(Vector3.forward * Random.Range(0f, 360f), Space.Self);
+        _speed = Random.Range(5f, 10f);
+        status = (StatusType) Random.Range(0, Enum.GetValues(typeof(StatusType)).Length);
+        _age = Random.Range(0, 61);
     }
 
     private float CheckImmunity()
@@ -49,13 +59,15 @@ public class SubjectController : MonoBehaviour
     }
 
 
-    private void ChangeDirection()
+    private void ChangeDirection(float amount = 180f)
     {
-        transform.Rotate(Vector3.forward * 180, Space.Self);
+        transform.Rotate(Vector3.forward * amount, Space.Self);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        ChangeDirection();
+        if (other.transform.CompareTag("Subject"))
+            ChangeDirection(Random.Range(0f, 360f));
+        else ChangeDirection();
     }
 }
